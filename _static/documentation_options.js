b1624b777a04d04133e38200521c23031c30bceb
Moisès Gómez <moises.gomez@iesjoandaustria.org>
2020-09-01 22:23:49 +0200

    automated commit on dt. de set.  1 22:23:49 CEST 2020

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2020-21',
+    VERSION: '1819',
     LANGUAGE: 'ca',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
